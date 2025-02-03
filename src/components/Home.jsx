df13ce8f366dfc7983482049d11520aaff812282
Joseph Vega <javega175@gmail.com>
2025-02-02 20:27:23 -0800

    added styling to heading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,31 +10,53 @@ export default function Home() {
   const subheading = useRef();
 
   useGSAP(() => {
-    const timeline = gsap.timeline();
-    timeline.to(heading.current, {
-      text: {
-        value: home.frontmatter.title,
-      },
-      duration: 1,
-      delay: 0.5,
-      ease: "none",
-    });
-
-    timeline.to(subheading.current, {
-      text: {
-        value: home.frontmatter.subheading,
-      },
-      duration: 1,
-      delay: 0.5,
-      ease: "none",
-    });
+    // const timeline = gsap.timeline();
+    // timeline.to(heading.current, {
+    //   text: {
+    //     value: home.frontmatter.title,
+    //   },
+    //   duration: 1,
+    //   delay: 0.5,
+    //   ease: "none",
+    // });
+    // timeline.to(subheading.current, {
+    //   text: {
+    //     value: home.frontmatter.subheading,
+    //   },
+    //   duration: 1,
+    //   delay: 0.5,
+    //   ease: "none",
+    // });
   });
 
   return (
     <section className="home">
       <div className="grid-heading">
-        <h1 className="heading" ref={heading}></h1>
-        <h3 className="subheading" ref={subheading}></h3>
+        <div className="top-content">
+          <div className="left-avatar-box"></div>
+          <div className="right-text">
+            <h1 className="heading">
+              {home.frontmatter.title}{" "}
+              <span className="teal-color">{home.frontmatter.name}</span>
+            </h1>
+            <h3 className="subheading">{home.frontmatter.subheading}</h3>
+          </div>
+        </div>
+
+        <div className="bottom-description">
+          <p>{home.frontmatter.welcomeText}</p>
+          <br />
+          <p>{home.frontmatter.aboutText}</p>
+        </div>
+      </div>
+      <div className="tabs">
+        <h3>{home.frontmatter.pagesTitle}</h3>
+        {home.frontmatter.pages.map(({ title, description }) => (
+          <div className="tab">
+            <h4>{title}</h4>
+            <p>{description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/home/home.md
+++ src/home/home.md
@@ -1,7 +1,10 @@
 ---
-title: "Hi! Im Joseph,"
+title: "Hi! I'm"
+name: "Joseph,"
 subheading: "A full stack software engineer.."
-description: "Als full-stack Javascript developer bouw ik moderne web-apps met tools als React.js, Next.js & Tailwind CSS."
+welcomeText: "Welcome to my personal (developer) page!"
+aboutText: "Here I plan to save things about me, like what music I like listening to, my personal projects, my story and much more."
+pagesTitle: "Pages"
 pages:
   [
     { title: "Project 1", description: "A Short Description of the project." },
